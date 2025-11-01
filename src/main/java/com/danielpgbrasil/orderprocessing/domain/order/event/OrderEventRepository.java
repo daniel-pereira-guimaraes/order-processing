4ca1d4e81ff508306ef72fc91f30c5f92aae2312
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2025-11-01 10:12:24 -0400

    Criada a interface OrderEventRepository com métodos save, get, getOrThrow e findAllUnpublished

---
+++ src/main/java/com/danielpgbrasil/orderprocessing/domain/order/event/OrderEventRepository.java
@@ -0,0 +1,11 @@
+package com.danielpgbrasil.orderprocessing.domain.order.event;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface OrderEventRepository {
+    void save(OrderEvent orderEvent);
+    Optional<OrderEvent> get(OrderEventId id);
+    OrderEvent getOrThrow(OrderEventId id);
+    List<OrderEvent> findAllUnpublished();
+}
