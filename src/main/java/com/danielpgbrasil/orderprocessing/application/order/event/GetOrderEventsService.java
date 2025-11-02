4e91a0beafd443a2781eb785e568a616f109b984
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2025-11-02 17:45:22 -0400

    Verificação da existência do pedido em GetOrderEventsService; e testes

--- src/main/java/com/danielpgbrasil/orderprocessing/application/order/event/GetOrderEventsService.java
+++ src/main/java/com/danielpgbrasil/orderprocessing/application/order/event/GetOrderEventsService.java
@@ -1,26 +1,28 @@
 package com.danielpgbrasil.orderprocessing.application.order.event;
 
 import com.danielpgbrasil.orderprocessing.domain.order.OrderId;
+import com.danielpgbrasil.orderprocessing.domain.order.OrderNotFoundException;
+import com.danielpgbrasil.orderprocessing.domain.order.OrderRepository;
 import com.danielpgbrasil.orderprocessing.domain.order.event.OrderEvent;
 import com.danielpgbrasil.orderprocessing.domain.order.event.OrderEventRepository;
 
 import java.util.List;
 
-/* Este service poderia ser suprimido, pois não há nenhum processamento
-    relevante sendo executado aqui. O controller poderia obter os eventos
-    diretamente do repositório. Mesmo assim optei por criar este service
-    porque fica mais fácil evoluir caso surja necessidade de algum
-    processamento adicional, como validação de acesso, por exemplo.
- */
 public class GetOrderEventsService {
 
-    private final OrderEventRepository repository;
+    private final OrderRepository orderRepository;
+    private final OrderEventRepository orderEventRepository;
 
-    public GetOrderEventsService(OrderEventRepository repository) {
-        this.repository = repository;
+    public GetOrderEventsService(OrderRepository orderRepository,
+                                 OrderEventRepository orderEventRepository) {
+        this.orderRepository = orderRepository;
+        this.orderEventRepository = orderEventRepository;
     }
 
     public List<OrderEvent> getEvents(OrderId orderId) {
-        return repository.findByOrderId(orderId);
+        if (!orderRepository.exists(orderId)) {
+            throw new OrderNotFoundException(orderId);
+        }
+        return orderEventRepository.findByOrderId(orderId);
     }
 }
--- src/test/java/com/danielpgbrasil/orderprocessing/ut/application/order/event/GetOrderEventsServiceTest.java
+++ src/test/java/com/danielpgbrasil/orderprocessing/ut/application/order/event/GetOrderEventsServiceTest.java
@@ -7,10 +7,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 import com.danielpgbrasil.orderprocessing.application.order.event.GetOrderEventsService;
+import com.danielpgbrasil.orderprocessing.domain.order.OrderNotFoundException;
+import com.danielpgbrasil.orderprocessing.domain.order.OrderRepository;
 import com.danielpgbrasil.orderprocessing.domain.order.event.OrderEvent;
 import com.danielpgbrasil.orderprocessing.domain.order.event.OrderEventRepository;
 import com.danielpgbrasil.orderprocessing.fixture.OrderEventFixture;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -22,15 +23,19 @@ class GetOrderEventsServiceTest {
             OrderEventFixture.builder().build()
     );
 
-    private OrderEventRepository repository;
+    private OrderRepository orderRepository;
+    private OrderEventRepository orderEventRepository;
     private GetOrderEventsService service;
 
     @BeforeEach
     void beforeEach() {
-        repository = mock(OrderEventRepository.class);
-        service = new GetOrderEventsService(repository);
+        orderRepository = mock(OrderRepository.class);
+        orderEventRepository = mock(OrderEventRepository.class);
 
-        when(repository.findByOrderId(ORDER_ID)).thenReturn(EXPECTED_EVENTS);
+        service = new GetOrderEventsService(orderRepository, orderEventRepository);
+
+        when(orderRepository.exists(ORDER_ID)).thenReturn(true);
+        when(orderEventRepository.findByOrderId(ORDER_ID)).thenReturn(EXPECTED_EVENTS);
     }
 
     @Test
@@ -38,15 +43,29 @@ class GetOrderEventsServiceTest {
         var events = service.getEvents(ORDER_ID);
 
         assertThat(events, is(EXPECTED_EVENTS));
-        verify(repository).findByOrderId(ORDER_ID);
+        verify(orderRepository).exists(ORDER_ID);
+        verify(orderEventRepository).findByOrderId(ORDER_ID);
     }
 
     @Test
-    void propagatesExceptionWhenRepositoryFails() {
-        when(repository.findByOrderId(ORDER_ID)).thenThrow(RuntimeException.class);
+    void propagatesOrderNotFoundExceptionWhenOrderDoesNotExist() {
+        when(orderRepository.exists(ORDER_ID)).thenReturn(false);
+
+        assertThrows(OrderNotFoundException.class,
+                () -> service.getEvents(ORDER_ID)
+        );
+
+        verify(orderRepository).exists(ORDER_ID);
+        verifyNoInteractions(orderEventRepository);
+    }
+
+    @Test
+    void propagatesRuntimeExceptionWhenRepositoryFails() {
+        when(orderEventRepository.findByOrderId(ORDER_ID)).thenThrow(RuntimeException.class);
 
         assertThrows(RuntimeException.class, () -> service.getEvents(ORDER_ID));
 
-        verify(repository).findByOrderId(ORDER_ID);
+        verify(orderRepository).exists(ORDER_ID);
+        verify(orderEventRepository).findByOrderId(ORDER_ID);
     }
 }
