fe26f04d60e2d469b501e99b0c3fb55925f38679
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2025-11-02 21:54:31 -0400

    Adicionado modulo OrderEventDeserializer ao ObjectMapper em JacksonConfig

--- src/main/java/com/danielpgbrasil/orderprocessing/infrastructure/config/JacksonConfig.java
+++ src/main/java/com/danielpgbrasil/orderprocessing/infrastructure/config/JacksonConfig.java
@@ -2,6 +2,7 @@ package com.danielpgbrasil.orderprocessing.infrastructure.config;
 
 import com.danielpgbrasil.orderprocessing.domain.order.OrderDetails;
 import com.danielpgbrasil.orderprocessing.domain.order.event.OrderEvent;
+import com.danielpgbrasil.orderprocessing.infrastructure.shared.OrderEventDeserializer;
 import com.danielpgbrasil.orderprocessing.infrastructure.shared.OrderEventSerializer;
 import com.danielpgbrasil.orderprocessing.infrastructure.shared.OrderDetailsSerializer;
 import com.danielpgbrasil.orderprocessing.infrastructure.shared.OrderDetailsDeserializer;
@@ -15,9 +16,10 @@ public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        SimpleModule module = new SimpleModule();
+        var mapper = new ObjectMapper();
+        var module = new SimpleModule();
         module.addSerializer(OrderEvent.class, new OrderEventSerializer());
+        module.addDeserializer(OrderEvent.class, new OrderEventDeserializer());
         module.addSerializer(OrderDetails.class, new OrderDetailsSerializer());
         module.addDeserializer(OrderDetails.class, new OrderDetailsDeserializer());
         mapper.registerModule(module);
