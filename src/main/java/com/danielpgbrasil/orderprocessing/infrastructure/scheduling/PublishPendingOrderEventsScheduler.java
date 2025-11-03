ee57e2911664778bc21efc532b075888e3515c69
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2025-11-02 20:20:45 -0400

    Agendamento da publicação de eventos pendentes

---
+++ src/main/java/com/danielpgbrasil/orderprocessing/infrastructure/scheduling/PublishPendingOrderEventsScheduler.java
@@ -0,0 +1,20 @@
+package com.danielpgbrasil.orderprocessing.infrastructure.scheduling;
+
+import com.danielpgbrasil.orderprocessing.application.order.event.PublishPendingOrderEventsService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PublishPendingOrderEventsScheduler {
+
+    private final PublishPendingOrderEventsService publishService;
+
+    public PublishPendingOrderEventsScheduler(PublishPendingOrderEventsService publishService) {
+        this.publishService = publishService;
+    }
+
+    @Scheduled(fixedDelay = 5000L)
+    public void run() {
+        publishService.publishPendingEvents();
+    }
+}
