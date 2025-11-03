132e8de75d44350a493f4ce2d3433ed229cfbac0
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2025-11-02 20:17:33 -0400

    Configuração do Jeckson com serializadores/desserializadores customizados

---
+++ src/main/java/com/danielpgbrasil/orderprocessing/infrastructure/config/JacksonConfig.java
@@ -0,0 +1,26 @@
+package com.danielpgbrasil.orderprocessing.infrastructure.config;
+
+import com.danielpgbrasil.orderprocessing.domain.order.OrderDetails;
+import com.danielpgbrasil.orderprocessing.domain.order.event.OrderEvent;
+import com.danielpgbrasil.orderprocessing.infrastructure.shared.OrderEventSerializer;
+import com.danielpgbrasil.orderprocessing.infrastructure.shared.OrderDetailsSerializer;
+import com.danielpgbrasil.orderprocessing.infrastructure.shared.OrderDetailsDeserializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(OrderEvent.class, new OrderEventSerializer());
+        module.addSerializer(OrderDetails.class, new OrderDetailsSerializer());
+        module.addDeserializer(OrderDetails.class, new OrderDetailsDeserializer());
+        mapper.registerModule(module);
+        return mapper;
+    }
+}
