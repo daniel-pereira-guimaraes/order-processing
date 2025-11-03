444a2284a20fc20c5a51fd470977a719f9ca8d38
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2025-11-02 21:53:02 -0400

    Criado bean de MessageConverter para usar ObjectMapper, em RabbitMqConfig

--- src/main/java/com/danielpgbrasil/orderprocessing/infrastructure/config/RabbitMqConfig.java
+++ src/main/java/com/danielpgbrasil/orderprocessing/infrastructure/config/RabbitMqConfig.java
@@ -1,6 +1,9 @@
 package com.danielpgbrasil.orderprocessing.infrastructure.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,6 +11,13 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMqConfig {
 
     public static final String ORDER_EVENTS_ROUTING_KEY = "order-events";
+
+    @Bean
+    public MessageConverter jsonMessageConverter(ObjectMapper objectMapper) {
+        var converter = new Jackson2JsonMessageConverter(objectMapper);
+        converter.setCreateMessageIds(true);
+        return converter;
+    }
 
     @Bean
     Queue orderQueue() {
